15289924db74898be0d23de2daf9b1883e56f032
Loumann <bleyka80@gmail.com>
2023-02-16 01:17:14 +0500

    задание 2

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq;
 
+//задание 2
 namespace Task_1
 {
     class Program
@@ -8,45 +9,65 @@ namespace Task_1
         static void Main(string[] args)
         {
             string line;
+            char[] charsEnglish = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
+            string wrongChars = "";
+
+
             line = Console.ReadLine();
 
 
-            Console.WriteLine(line.Length);
+            char[] inputLine = line.ToCharArray();
 
-
-            if ((line.Length % 2) == 0)
+            foreach (char c in inputLine)
             {
-                Console.WriteLine("Четное");
-
-                string str1 = line.Substring(0, line.Length / 2);   
-                string str2 = line.Substring(line.Length / 2, line.Length / 2);
-
-
-                char[] charArray1 = str1.ToCharArray();
-                char[] charArray2 = str2.ToCharArray();
-                Array.Reverse(charArray1);
-                Array.Reverse(charArray2);
-
-
-                string s1 = new string(charArray1);
-                string s2 = new string(charArray2);
-                Console.WriteLine(s1 + s2);
+                if (!charsEnglish.Contains(c))
+                {
+                    wrongChars += c + " ";
+                }
             }
+
+            if (wrongChars != "")
+            {
+                Console.WriteLine("Символы не прошедшую проверку: {0}", wrongChars);
+            }
+
+
             else
             {
+                if ((line.Length % 2) == 0)
+                {
+                    Console.WriteLine("Четное. Количество: " + line.Length);
 
-                Console.WriteLine("не Четное");
+                    string str1 = line.Substring(0, line.Length / 2);
+                    string str2 = line.Substring(line.Length / 2, line.Length / 2);
 
-                char[] charArray1 = line.ToCharArray();
-                Array.Reverse(charArray1);
-                string a1 = new string(charArray1);
 
-                Console.WriteLine(a1 + line);
+                    char[] charArray1 = str1.ToCharArray();
+                    char[] charArray2 = str2.ToCharArray();
+                    Array.Reverse(charArray1);
+                    Array.Reverse(charArray2);
 
-                //наверное замудрил сильно, но я не понял немного как 2 char[] соеденить(что-то такое вроде было,я уже забыл если честно что там было :)), траблы были с выводом
-                //в общем и целом, не получилось у меня немного, я поэтому я решил особо не парится и сделать так
+
+                    string s1 = new string(charArray1);
+                    string s2 = new string(charArray2);
+                    Console.WriteLine(s1 + s2);
+                    return;
+                }
+                else
+                {
+
+                    Console.WriteLine("Не четное. Количество: " + line.Length);
+
+                    char[] charArray1 = line.ToCharArray();
+                    Array.Reverse(charArray1);
+                    string a1 = new string(charArray1);
+
+                    Console.WriteLine(a1 + line);
+                    return;
+
+
+                }
             }
-
         }
     }
 }
